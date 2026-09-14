e81b7e2e26735d12bfd5e310dc008b405771875c
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-09-15 00:34:31 +0530

    feat: create about me section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import firstPotrait from './assets/images/alexander-london-mJaD10XeD7w-unsplash.jpg';
+
+const firstPotraitHolder = document.querySelector('.potrait-holder');
+const firstPotraitImg = new Image();
+firstPotraitImg.src = firstPotrait;
+firstPotraitImg.classList.add('.first-potrait-img');
+firstPotraitHolder.appendChild(firstPotraitImg);

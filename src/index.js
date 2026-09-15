6ce6e6f68d072c1c8e03844efe5bb26f3ec301c1
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-09-15 23:51:23 +0530

    feat: make it tablet responsive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import './style.css';
-import firstPotrait from './assets/images/alexander-london-mJaD10XeD7w-unsplash.jpg';
+import firstPotraitTall from './assets/images/firstCatTall.png';
+import firstPotraitWide from './assets/images/firstCatWide.png';
 
 const firstPotraitHolder = document.querySelector('.potrait-holder');
+const pictureElement = document.createElement('picture');
+const tallSource = document.createElement('source');
+tallSource.srcset = firstPotraitTall;
+tallSource.media = '(max-width: 800px)';
+pictureElement.appendChild(tallSource);
+const wideSource = document.createElement('source');
+wideSource.srcset = firstPotraitWide;
+wideSource.media = '(min-width: 800px)';
+pictureElement.appendChild(wideSource);
 const firstPotraitImg = new Image();
-firstPotraitImg.src = firstPotrait;
-firstPotraitImg.classList.add('.first-potrait-img');
-firstPotraitHolder.appendChild(firstPotraitImg);
+firstPotraitImg.src = firstPotraitTall;
+firstPotraitImg.classList.add('first-potrait-img');
+pictureElement.appendChild(firstPotraitImg);
+firstPotraitHolder.appendChild(pictureElement);
